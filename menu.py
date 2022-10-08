cd5a831b39d6f4f5ff4c2213cc19384926a53bee
Jan22002 <jan_kakko@hotmail.com>
2022-10-08 19:16:46 +0300

    Projektin aloitus tiedostot

---
+++ menu.py
@@ -0,0 +1,29 @@
+import string
+
+
+class Ravintola:
+    alkupalat = []
+    paamenu  = []
+    jalkiruoka = []
+    juomat = []
+
+    lasku = 0
+
+   
+
+    def Alkupalat(self, alkupalat):
+        alkupalat = [] 
+        
+    def Paamenu(self, paamenu):
+        paamenu = []
+
+    def Jalkiruoka(self, jalkiruoka):
+        jalkiruoka = []
+    
+    def Juomat(self,juomat):
+        juomat = []
+
+    
+    def Lasku(self, lasku):
+        while True:
+            lasku = (string(input))
